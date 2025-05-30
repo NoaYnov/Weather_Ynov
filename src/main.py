ba03ce6e46681edd74b5d06125bc4deb02ee76d5
gambeynoa <113116077+NoaYnov@users.noreply.github.com>
2025-05-30 18:28:15 +0200

    ...

--- src/main.py
+++ src/main.py
@@ -2,9 +2,13 @@ import lib.api_station as api_station
 import lib.api_temperature as api_temperature
 
 def main():
+<<<<<<< Updated upstream
     # api_station.main()
     api_temperature.main()
     
+=======
+    test_api.test_api()
+>>>>>>> Stashed changes
     
     
 if __name__ == "__main__":
