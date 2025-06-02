9acce62460a88c8545489c906501700c4021e1b3
gambeynoa <113116077+NoaYnov@users.noreply.github.com>
2025-06-02 12:54:53 +0200

    donne meteo ynpv aix

--- src/main.py
+++ src/main.py
@@ -2,13 +2,9 @@ import lib.api_station as api_station
 import lib.api_temperature as api_temperature
 
 def main():
-<<<<<<< Updated upstream
     # api_station.main()
     api_temperature.main()
-    
-=======
-    test_api.test_api()
->>>>>>> Stashed changes
+
     
     
 if __name__ == "__main__":
